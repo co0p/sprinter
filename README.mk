fe523d71d19ce246d00ab8b8d56d786282d6f768
Julian Godesa <julian.godesa@medx.net>
2014-03-28 19:19:37 +0100

    added readme

---
+++ README.mk
@@ -0,0 +1,12 @@
+Sprinter
+=========
+
+A little (angular) website helping a scrummaster to get an understanding
+of the available time to plan a sprint.
+
+run the app with
+   grunt serve
+
+
+The server is a little php script serving and saving the db.
+   cd server; php -S 127.0.0.1:8080
