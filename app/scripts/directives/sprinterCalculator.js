47f4fa1e31f7b0009cee2dfa92dee4a47e3e8cdf
Julian Godesa <julian.godesa@medx.net>
2014-03-31 15:02:01 +0200

    group stats are now being displayed

--- app/scripts/directives/sprinterCalculator.js
+++ app/scripts/directives/sprinterCalculator.js
@@ -38,11 +38,52 @@ angular.module('sprinterApp')
           return total.toFixed(1);
         }
 
+        // calculates the current stats for the individual groups of one team
+        function groupStats() {
+          var groups = [];
+
+          function getGroupIndex(group) {
+            for (var pos in groups) {
+              if (groups[pos].name === group) {
+                return pos;
+              }
+            }
+            return -1;
+          }
+
+          for (var pos in activeTeam.members) {
+            var member = activeTeam.members[pos];
+
+            var candidate = member.group;
+            var index = getGroupIndex(candidate);
+
+            // add new group if not found, else add to hours count
+            if (index < 0) {
+              groups.push({
+                name: candidate,
+                hours: member.days * member.drag * HOURS_IN_DAY,
+                memberCount: 1
+              });
+            } else {
+              groups[index].hours += member.days * member.drag * HOURS_IN_DAY;
+              groups[index].memberCount += 1;
+            }
+          }
+
+          // make sure that the numbers are fixed
+          groups.forEach(function(group) {
+            group.hours = group.hours.toFixed(1);
+          });
+
+          return groups;
+        }
+
         // updates the team stats
         function updateStats() {
           $scope.averageDays = averageDays();
           $scope.averageDrag = averageDrag();
           $scope.totalHours = totalHours();
+          $scope.groups = groupStats();
         }
 
         // get a handle for the currently active team
@@ -53,10 +94,7 @@ angular.module('sprinterApp')
         $scope.days = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
         $scope.drags = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
 
-        $scope.averageDays = averageDays();
-        $scope.averageDrag = averageDrag();
-        $scope.totalHours = totalHours();
-
+        updateStats();
 
         // user wants to delete a member
         $scope.deleteMember = function(member) {
